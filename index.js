db549b0de4b4e19825e301fc3c9e0198e44b6c2f
Sơn Nguyễn <106434538+1402sn@users.noreply.github.com>
2024-02-14 13:32:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,16 +21,18 @@ const r = new Discord.RichPresence()
     .setType('PLAYING') // You can Set Any other like PLAYING, LISTENING, STREAMING
     .setURL('https://www.youtube.com/@Twqz7') //set any youtube or twitch link
   
-    .setName('awsxu') // add your name
-    .setDetails('Active in Launcher') // set your status
-    .setState('Version: 0.9')
+    .setName('Codee') // add your name
+    .setDetails('Idling') // set your status
+    .setState(null)
   
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://media.discordapp.net/attachments/1194705818147373078/1203379587543859312/pngegg.png') // add image link (form: .png, .gif).
-    .setAssetsLargeText('awsxu') // set image name (type any name)
+    .setAssetsLargeImage('https://media.discordapp.net/attachments/1116694463658598412/1203880471084138506/idle-vscode.png?ex=65dbee8f&is=65c9798f&hm=e5a4c84b14058b8283f050b36ae419f41c1171341b6409b7115e6bba3bb02e79&=&format=webp&quality=lossless&width=473&height=473') // add image link (form: .png, .gif).
+    .setAssetsLargeText('Idling') // set image name (type any name)
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1116694463658598412/1203880477870653440/idle.png?ex=65dbee90&is=65c97990&hm=21dee6be5f9850a571e87da31f963c29aadd97efb01d7311c4b19c63822f5d4f&=&format=webp&quality=lossless&width=473&height=473') 
+    .setAssetsSmallText('zZz')
     .addButton('Discord', 'https://discord.gg/t7EPCNYdMw')//  // add button name and paste the link like discord server, youtube channel, etc.
      client.user.setActivity(r);
-     client.user.setPresence({ status: "online" }); // set your status (online,dnd, idle)
+     client.user.setPresence({ status: "idle" }); // set your status (online,dnd, idle)
 })
 
 client.login(process.env.TOKEN)
